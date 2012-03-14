f5698bc6860eda97e589bbe993c0b6ce7c87056f
Zach McCullough <nosrepa@gmail.com>
2012-03-13 22:40:46 -0500

    Now that the reddit api can tell if a submission/comment was removed, we can now check for that and remod the bot

--- SuggestionBot.py
+++ SuggestionBot.py
@@ -166,7 +166,8 @@ def main():
     if top_comments:
         count = 0
         for i in top_comments:
-            if i['author'] != '[deleted]':
+            #if i['author'] != '[deleted]': # We're a mod now, so we don't need this
+            if i['banned_by']: # If the submission is removed, it'll be None
                 count += 1
                 if i['author_flair_css_class']:
                     flair = flairs[i['author_flair_css_class']]
